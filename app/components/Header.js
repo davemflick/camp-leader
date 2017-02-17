077235ebf9d2c1e013e19d7936c6ff6aa590af5a
davemflick <davemflick@yahoo.com>
2017-02-17 12:08:36 -0500

    Basic setup

---
+++ app/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+	render () {
+		return (
+			<div className='headerContainer'>
+			  <h1> FreeCodeCamp Leader Board </h1>
+			</div>
+		)
+	}
+}
---
+++ app/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import TableContainer from './components/TableContainer';
+import Header from './components/Header';
+import styles from "!style-loader!css-loader!sass-loader!./styles/main.css";
+
+
+class App extends React.Component {
+	render () {
+		return (
+			<div>
+			 <Header />
+			 <TableContainer />
+			</div>
+		)
+	}
+}
+
+render(<App />, document.getElementById('start'));
